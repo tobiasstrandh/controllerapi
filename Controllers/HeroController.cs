7688af1a759f2de5232832a2ac8366664a6deded
tobiasstrandh <tobias.strandh@iths.se>
2023-02-15 13:12:09 +0100

    hej

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.CompilerServices;
+using democloudrobin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace democloudrobin.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HeroController : ControllerBase
+	{
+
+		private static List<Hero> _heroes = new List<Hero>()
+		{
+			new Hero(){Id=1, Name = "Tony Stark", Team = "Avengers", SuperHeroName = "Iron Man"},
+			new Hero(){Id=2, Name = "Bruce Wayne", Team = "Justice League", SuperHeroName = "Batman"}
+		};
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Ok(_heroes);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok(_heroes.FirstOrDefault(x => x.Id.Equals(id)));
+		}
+
+		[HttpPost]
+		public IActionResult Add(Hero hero)
+		{
+			_heroes.Add(hero);
+			return Ok();
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(Hero hero)
+		{
+			var heroToUpdate = _heroes.FirstOrDefault(x => x.Id == hero.Id);
+			if (heroToUpdate == null)
+			{
+				return NotFound();
+			}
+
+			heroToUpdate.Name = hero.Name;
+			heroToUpdate.Team = hero.Team;
+			heroToUpdate.SuperHeroName = hero.SuperHeroName;
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var heroToDelete = _heroes.FirstOrDefault(x => x.Id == id);
+			if (heroToDelete == null)
+			{
+				return NotFound();
+			}
+
+			_heroes.Remove(heroToDelete);
+			return Ok();
+		}
+	}
+}
---
+++ Models/Hero.cs
@@ -0,0 +1,11 @@
+﻿namespace democloudrobin.Models;
+
+public class Hero
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string SuperHeroName { get; set; }
+	public string Team { get; set; }
+
+
+}
